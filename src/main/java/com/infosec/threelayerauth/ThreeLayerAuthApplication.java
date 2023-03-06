f7ab2cd105c9e30a59df8b1ef477c0cbae4e3b0d
radulovskia <85944330+somefinkistudent@users.noreply.github.com>
2023-03-06 20:36:12 +0100

    Initial commit

---
+++ src/main/java/com/infosec/threelayerauth/ThreeLayerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.infosec.threelayerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeLayerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeLayerAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosec/threelayerauth/ThreeLayerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosec.threelayerauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeLayerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
